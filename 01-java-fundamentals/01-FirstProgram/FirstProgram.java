f33d2501ed52bf5359306e5a15f1eb53ab7556de
Damon Benz <benzdj11@uww.edu>
2021-04-07 18:37:59 -0500

    First Program

---
+++ 01-java-fundamentals/01-FirstProgram/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Damon Benz");
+        System.out.println("24");
+        System.out.println("Green Bay, WI");
+    }
+}
